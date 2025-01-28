d35ee90b6dbfb14004fd60a64d1f68ee4268e207
cj <jb.carl@outlook.com>
2025-01-28 13:25:48 -0700

    feat: remove about url

--- chant/urls.py
+++ chant/urls.py
@@ -5,5 +5,4 @@ from . import views
 urlpatterns = [
     path('', views.index, name="chant-index"),
     path('chant/', views.home, name="chant-home"),
-    path('about/', views.about, name="chant-about"),
 ]
