123db00d9a73f7b7ad17b0a5efde9ce61a1b1699
cj <jb.carl@outlook.com>
2025-01-30 18:24:39 -0700

    feat: design database model

--- chant/models.py
+++ chant/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
 # Create your models here.
+class Song(models.Model):
+    title = models.CharField(max_length=255)
+    
+    def __str__(self):
+        return self.title
+    
+class Section(models.Model):
+    TYPE_CHOICES = [
+        ('verse', 'Verse'),
+        ('chorus', 'Chorus')
+    ]
+
+    song = models.ForeignKey(Song, related_name='sections', on_delete=models.CASCADE)
+    section_type = models.CharField(max_length=10, choices=TYPE_CHOICES)
+
+    def __str__(self):
+        return f"{self.section_type} - {self.song.title}"
+    
+
+class Line(models.Model):
+    section = models.ForeignKey(Section, related_name='lines', on_delete=models.CASCADE)
+    content = models.TextField()
+
+    def __str__(self):
+        return self.content
