0d8f2c004e61230bf90a7be3f4961978aebb8116
cj <jb.carl@outlook.com>
2025-02-01 11:28:42 -0700

    modify url path

--- chant/urls.py
+++ chant/urls.py
@@ -3,8 +3,8 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.base, name="chant_base"),
-    path('chant/', views.home, name="chant-home"),
-    path('song/', views.song_list, name="song_list"),
+    path('', views.base, name="r_base"),
+    path('chant/', views.chant_home, name="chant-home"),
+    path('chant/song/', views.song_list, name="song_list"),
     path('song/<int:song_id>/', views.song_detail, name="song_detail")
 ]
