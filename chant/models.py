622148b616ac016f6aa9f8476c17021f5209743b
cj <jb.carl@outlook.com>
2025-02-02 00:40:48 -0700

    feat added lyrical category

--- chant/models.py
+++ chant/models.py
@@ -1,9 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.name
+    
+
 class Song(models.Model):
     id = models.AutoField(primary_key=True, null=False)
     title = models.CharField(max_length=255)
+    categories = models.ManyToManyField(Category, related_name="songs")
     
     def __str__(self):
         return self.title
