72f35d9d74b09bbf3bb3cdaef5b54d5ecada43af
cj <jb.carl@outlook.com>
2025-02-19 01:17:09 -0700

    feat: song_list displaying list of songs base on category selection

--- chant/views.py
+++ chant/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
-from .models import Song, Section, Line
+from .models import Song, Section, Line, Category, Language
 
 
 
@@ -8,20 +8,21 @@ from .models import Song, Section, Line
 def base(request):
     return render(request, 'chant/base.html')
 
-# List of songs (with verses and choruses) in the desired format
 
-def song_list(request):
+def song_list(request, category_name):
     """ View to display the list of all songs"""
-    songs = Song.objects.all()
+    songs = Song.objects.filter(category_id = category_name).order_by('category_number')
     return render(request, 'chant/song_list.html', {'songs': songs})
 
 
-def song_detail(request, song_id):
+def song_detail(request, category_number):
     """ View to display a songle song with it's sections and lines """
-    song = get_object_or_404(Song, id=song_id)
+    song = Song.objects.get(category_number=category_number)
+    verse_counter = 0
 
-    return render(request, 'chant/song_detail.html', {'song': song})
+    return render(request, 'chant/song_detail.html', {'song': song, 'verse_counter': verse_counter})
 
 
 def home(request):
     return HttpResponse("<h1>Hello!!!</h1>")
+
