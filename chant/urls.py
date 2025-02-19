c15ba746b82174b8e6284aa8aed11f6c57041fe3
cj <jb.carl@outlook.com>
2025-02-19 01:34:03 -0700

    feat: updated url path for dynamic route

--- chant/urls.py
+++ chant/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.base, name="r_base"),
-    path('chant/', views.chant_home, name="chant-home"),
-    path('chant/song/', views.song_list, name="song_list"),
-    path('song/<int:song_id>/', views.song_detail, name="song_detail")
+    path('chant/', views.chant, name="chant-home"),
+    path('chant/<str:category_name>/', views.song_list, name="song_list"),
+    path('chant/song/<int:category_number>/', views.song_detail, name="song_detail")
 ]
