5cd24c508a98df8ec800eac3ce65d1db85cdc14f
cj <jb.carl@outlook.com>
2025-02-19 22:37:00 -0700

    feat: added slug for slugifying Category.name & Song.title

--- chant/models.py
+++ chant/models.py
@@ -1,9 +1,17 @@
 from django.db import models
+from django.utils.text import slugify
 
 # Create your models here.
 
 class Category(models.Model):
     name = models.CharField(primary_key=True, max_length=100, unique=True, null=False)
+    slug = models.SlugField(unique=True, blank=True)
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+            super().save(*args, **kwargs)
+    
 
     def __str__(self):
         return self.name
@@ -23,6 +31,13 @@ class Song(models.Model):
     category = models.ForeignKey(Category, related_name="songcategory", on_delete=models.CASCADE)
     category_number = models.IntegerField()
     language = models.ForeignKey(Language, related_name="languages", on_delete=models.CASCADE)
+
+    slug = models.SlugField(unique=True, blank=True)
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)
+            super().save(*args, **kwargs)
     
     def __str__(self):
         return self.title
