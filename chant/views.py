a81bab2b46e88dff750a5d86f86ddea9ce4e7f3f
cj <jb.carl@outlook.com>
2025-02-19 22:35:47 -0700

    feat: added slug to slugigy url for song_detail & song_list
    update song_list_view to get songs base on languages

--- chant/views.py
+++ chant/views.py
@@ -9,14 +9,18 @@ def base(request):
     return render(request, 'chant/base.html')
 
 
-def song_list(request, category_name):
+def song_list(request, category_slug):
     """ View to display the list of all songs"""
-    songs = Song.objects.filter(category_id = category_name).order_by('category_number')
-    return render(request, 'chant/song_list.html', {'songs': songs})
+    selected_language = request.GET.get('choice', 'creole')
+    #songs_in_selected_lang = Song.objects.filter(language__language_type=selected_language)
+    category = Category.objects.get(slug = category_slug)
+    songs = Song.objects.filter(language__language_type=selected_language, category=category).order_by("category_number")
+    return render(request, 'chant/song_list.html', {'songs': songs, 'selected_language': selected_language})
 
 
-def song_detail(request, category_number):
+def song_detail(request, category_slug, category_number):
     """ View to display a songle song with it's sections and lines """
+    category = Category.objects.get(slug = category_slug)
     song = Song.objects.get(category_number=category_number)
     verse_counter = 0
 
