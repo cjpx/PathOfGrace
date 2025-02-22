29faeba6441c4b861889f2cded61647c64098627
cj <jb.carl@outlook.com>
2025-02-22 13:41:10 -0700

    feat: modify song_list & song_detail for passing song slug into url

--- chant/views.py
+++ chant/views.py
@@ -11,21 +11,20 @@ def base(request):
 
 def song_list(request, category_slug):
     """ View to display the list of all songs"""
-    selected_language = request.GET.get('choice', 'creole')
+    selected_language = request.GET.get('choice', 'Créole')
     #songs_in_selected_lang = Song.objects.filter(language__language_type=selected_language)
     category = Category.objects.get(slug = category_slug)
     songs = Song.objects.filter(language__language_type=selected_language, category=category).order_by("category_number")
-    return render(request, 'chant/song_list.html', {'songs': songs, 'selected_language': selected_language})
+    return render(request, 'chant/song_list.html', {'songs': songs, 'selected_language': selected_language, 'category': category})
 
 
-def song_detail(request, category_slug, category_number):
+def song_detail(request, category_slug, slug):
     """ View to display a songle song with it's sections and lines """
     category = Category.objects.get(slug = category_slug)
-    song = Song.objects.get(category_number=category_number)
+    song = Song.objects.get(slug=slug, category=category)
     verse_counter = 0
 
     return render(request, 'chant/song_detail.html', {'song': song, 'verse_counter': verse_counter})
-
 
 def home(request):
     return HttpResponse("<h1>Hello!!!</h1>")
