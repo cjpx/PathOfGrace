5d1b90eb06a1528edab772ef74d966726fa00716
cj <jb.carl@outlook.com>
2025-02-22 20:12:17 -0700

    feat: added register view

--- users/views.py
+++ users/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
-
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from .forms import UserRegisterForm, UserUpdateForm, ProfileUpdateForm
+from .models import User, Profile
+from django.http import HttpResponse
 # Create your views here.
+
+def register(request):
+    if request.method == "POST":
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Your account has been created! You are now able to log in.')
+            return redirect('login')
+    else:
+        form = UserRegisterForm()
+
+    return render(request, "users/register.html", {'form': form})
