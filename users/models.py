e395cc827b266ee1f7338ba309898c8a9a13cd96
cj <jb.carl@outlook.com>
2025-02-27 23:58:51 -0700

    feat: added image fields to Profile

--- users/models.py
+++ users/models.py
@@ -6,7 +6,7 @@ from PIL import Image
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    image = models.ImageField(default='profile.jpg', upload_to='profile_images')
+    image = models.ImageField(default='profile.jpg', upload_to='images/uploads/profile')
 
     def __str__(self):
         return f"{self.user.username} Profile"
