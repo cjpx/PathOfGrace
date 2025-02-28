550bca54f9e82a7ba214e6f174b43ad1c14a9b58
cj <jb.carl@outlook.com>
2025-02-28 00:16:51 -0700

    feat: added profile view

--- users/views.py
+++ users/views.py
@@ -19,3 +19,31 @@ def register(request):
         form = UserRegisterForm()
 
     return render(request, "users/register.html", {'form': form})
+
+
+@login_required
+def profile(request, username):
+    user = get_object_or_404(User, username=username)  # Fetch user by username
+    profile = get_object_or_404(Profile, user=user)  # Get user's profile
+
+    if request.method == "POST":
+        u_form = UserUpdateForm(request.POST, instance=user)
+        p_form = ProfileUpdateForm(request.POST, request.FILES, instance=profile)
+
+        if u_form.is_valid() and p_form.is_valid():
+            u_form.save()
+            p_form.save()
+            messages.success(request, f'Your profile has been updated!')
+            return redirect('profile', username=user.username)  # Redirect to updated profile
+
+    else:
+        u_form = UserUpdateForm(instance=user)
+        p_form = ProfileUpdateForm(instance=profile)
+
+    context = {
+        'profile': profile,
+        'u_form': u_form,
+        'p_form': p_form,
+    }
+
+    return render(request, "users/profile.html", context)
