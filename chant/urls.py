c3302f5d93cdb78e58ec88ae007914421dc5d9a9
cj <jb.carl@outlook.com>
2025-03-01 18:34:09 -0700

    feat: modify update_category  urls

--- chant/urls.py
+++ chant/urls.py
@@ -6,5 +6,7 @@ urlpatterns = [
     path('', views.base, name="r_base"),
     path('chant/', views.chant, name="chant-home"),
     path('chant/<slug:category_slug>/', views.song_list, name="song_list"),
-    path('chant/<slug:category_slug>/<slug:slug>/', views.song_detail, name="song_detail")
+    path('chant/<slug:category_slug>/<slug:slug>/', views.song_detail, name="song_detail"),
+
+    path('user/<slug:category_slug>/', views.update_category, name='update-category'),
 ]
